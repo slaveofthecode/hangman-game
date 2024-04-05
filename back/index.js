8626d6d745192a4cf4f81498ab6158c04265ab44
Gustavo Lopez <gustavo@getkalto.com>
2024-04-05 17:36:02 -0300

    Back folder was added

---
+++ back/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express');
+const router = require('./routes.js');
+const { connectToDB } = require('./db.js');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send('Test API is Ok');
+}); 
+
+server.use('/words', router);
+
+const init = async () => {
+    await connectToDB();
+
+    const port = process.env.SERVER_PORT || 3001;
+    server.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+};
+
+init();
